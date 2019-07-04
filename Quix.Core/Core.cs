56a4f734cf8b0ce8519f249abaf7b11e88964bcf
Cornelius J. van Dyk <c@dtdn.com>
2019-07-04 10:51:06 -0400

    NuGet version1.0.27 - Added getRegistryKey(), getDotNETVersion(), getDotNET45PlusVersion() and convert45PlusVersion().

--- Quix.Core/Core.cs
+++ Quix.Core/Core.cs
@@ -128,5 +128,169 @@ namespace Quix
                 return "";
             }
         }
+
+        /// <summary>
+        /// Returns the base RegistryKey from the Windows registry.
+        /// </summary>
+        /// <param name="registryPath">The registry path beyond HKLM.</param>
+        /// <returns>Microsoft.Win32.RegistryKey</returns>
+        public static Microsoft.Win32.RegistryKey getRegistryKey(string registryPath)
+        {
+            return Microsoft.Win32.RegistryKey.OpenBaseKey(
+                    Microsoft.Win32.RegistryHive.LocalMachine,
+                    Microsoft.Win32.RegistryView.Registry32)
+                .OpenSubKey(registryPath);
+        }
+
+        /// <summary>
+        /// Private method to return .NET version string when version is 4.5+
+        /// </summary>
+        /// <returns>String containing the version</returns>
+        private static string getDotNET45PlusVersion()
+        {
+            const string registrySubKeyPath =
+                @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+            string returnString = "";
+            using (var registryBaseKey = getRegistryKey(registrySubKeyPath))
+            {
+                if (registryBaseKey != null && registryBaseKey
+                    .GetValue("Release") != null)
+                {
+                    returnString = 
+                        convert45PlusVersion((int)registryBaseKey
+                        .GetValue("Release"));
+                }
+                else
+                {
+                    returnString = $"Unable to detect .NET version!";
+                }
+            }
+            return returnString;
+        }
+
+        // Checking the version using >= enables forward compatibility.
+        /// <summary>
+        /// Private method to convert registry version number to real world
+        /// number.
+        /// </summary>
+        /// <param name="versionNumber">The registry version number.</param>
+        /// <returns>String containing the version number.</returns>
+        private static string convert45PlusVersion(int versionNumber)
+        {
+            if (versionNumber >= 528040)
+                return "4.8 or later";
+            if (versionNumber >= 461808)
+                return "4.7.2";
+            if (versionNumber >= 461308)
+                return "4.7.1";
+            if (versionNumber >= 460798)
+                return "4.7";
+            if (versionNumber >= 394802)
+                return "4.6.2";
+            if (versionNumber >= 394254)
+                return "4.6.1";
+            if (versionNumber >= 393295)
+                return "4.6";
+            if (versionNumber >= 379893)
+                return "4.5.2";
+            if (versionNumber >= 378675)
+                return "4.5.1";
+            if (versionNumber >= 378389)
+                return "4.5";
+            // This code should never execute. A non-null release key should
+            // mean that 4.5 or later is installed.
+            return "No 4.5 or later version detected";
+        }
+
+        /// <summary>
+        /// Public method to get the .NET version number.
+        /// </summary>
+        /// <returns>String containing the .NET version number.</returns>
+        public static string getDotNETVersion()
+        {
+            const string registrySubKeyPath =
+                @"SOFTWARE\Microsoft\NET Framework Setup\NDP\";
+            string returnString = "";
+            bool ver45PlusPresent = false;
+            using (var registryBaseKey = getRegistryKey(registrySubKeyPath))
+            {
+                foreach (var versionKeyName in registryBaseKey.GetSubKeyNames())
+                {
+                    if (versionKeyName == "v4")
+                    {
+                        returnString += "\n" + getDotNET45PlusVersion();
+                        ver45PlusPresent = true;
+                    }
+                    else if (versionKeyName.StartsWith("v4.0") && ver45PlusPresent)
+                    {
+                        continue;
+                    }
+                    else if (versionKeyName.StartsWith("v"))
+                    {
+                        Microsoft.Win32.RegistryKey versionKey = 
+                            registryBaseKey.OpenSubKey(versionKeyName);
+                        // Get the .NET Framework version value.
+                        var name = (string)versionKey.GetValue("Version", "");
+                        // Get the service pack (SP) number.
+                        var sp = versionKey.GetValue("SP", "").ToString();
+                        // Get the installation flag, or an empty string if 
+                        // there is none.
+                        var install = versionKey.GetValue("Install", "")
+                            .ToString();
+                        if (string.IsNullOrEmpty(install)) // No install info; 
+                                                           // it must be in a 
+                                                           // child subkey.
+                        {
+                            returnString += "\n" + $"{versionKeyName}  {name}";
+                        }
+                        else
+                        {
+                            if (!(string.IsNullOrEmpty(sp)) && install == "1")
+                            {
+                                returnString += "\n" + $"{name} SP{sp}";
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            continue;
+                        }
+                        foreach (var subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            Microsoft.Win32.RegistryKey subKey = 
+                                versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (!string.IsNullOrEmpty(name))
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "")
+                                .ToString();
+                            if (string.IsNullOrEmpty(install)) // No install
+                                                               // info, it 
+                                                               // must be 
+                                                               // later.
+                            {
+                                returnString += "\n" + $"{versionKeyName}" +
+                                    $"  {name}";
+                            }
+                            else
+                            {
+                                if (!(string.IsNullOrEmpty(sp))
+                                    && install == "1")
+                                {
+                                    returnString += "\n" + $"{subKeyName}" +
+                                        $"  {name}  SP{sp}";
+                                }
+                                else if (install == "1")
+                                {
+                                    returnString += "\n" + $"  {subKeyName}" +
+                                        $"  {name}";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(returnString);
+            return returnString;
+        }
     }
 }
